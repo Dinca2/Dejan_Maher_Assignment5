c5b8d5723b76971f0f1dda4fd28bf6f0745cebec
Dinca2 <82816428+Dinca2@users.noreply.github.com>
2022-04-19 13:24:05 -0400

    Create Sorting.cpp

---
+++ Sorting.cpp
@@ -0,0 +1,33 @@
+#include "Sorting.h"
+
+Sorting::Sorting(){
+  comparisons = 0;
+}
+
+void Sorting::Selection(int[] table) {
+
+}
+
+void Sorting::Merge(int[] table) {
+
+}
+  
+void Sorting::Heap(int[] table) {
+
+}
+  
+void Sorting::QuickFP(int[] table) {
+
+}
+  
+void Sorting::QuickRP(int[] table) {
+
+}
+  
+void Sorting::Split() {
+
+}
+  
+int Sorting::GetComparisons() {
+  return comparisons;
+}
